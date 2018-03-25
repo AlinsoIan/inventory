52800995eafa7c82719850e06b8e11ae598c8d9e
Yanzky <yanzkytootsky@gmail.com>
2018-03-25 11:42:44 +0800

    Yeah!

--- admin/category.php
+++ admin/category.php
@@ -7,7 +7,7 @@ $res = $conn->query($sql);
 $data = [];
 if ($res) {
     while ($row = $res->fetch_assoc()) {
-        $data[] =  "<option>" . $row['description'] . "</option>";
+        $data[] =  "<option value=" .$row['description'] .">" . $row['description'] . "</option>";
     }
 }
 
